b9b3a362da77c91e381f00fe0f5a4e1cd3abfa63
Curtis <508798816@qq.com>
2022-12-10 14:01:17 +0800

    Delete Jshift.v

--- Single/A_Single/Jshift.v
+++
@@ -1,9 +0,0 @@
-//shift combination
-module Jshift(
-    input   [25:0] inst,
-    input   [31:0] pc_add4,
-    output  [31:0] sh
-    ); 
-    parameter z = 2'b00;
-    assign sh = {pc_add4[31:28],inst[25:0],z};
-endmodule
